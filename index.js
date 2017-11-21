567c504452fc68dda42c6053a9a959b594a34735
carloskwan <carlos.araujo@kwantec.com>
2017-11-21 12:02:54 -0600

    make it better

--- index.js
+++ index.js
@@ -60,9 +60,9 @@ app.post('/webhook', function (req, res) {
     // The default welcome intent has been matched, welcome the user (https://dialogflow.com/docs/events#default_welcome_intent)
 
     'input.welcome': () => {
-      /*
+      
       var options = {
-        uri: 'http://187.189.90.215:8585/v1/userRegistration',
+        uri: 'http://hope.westus.cloudapp.azure.com:8585/v1/userRegistration',
         headers: {
           'Accept': 'application/json'
       }
@@ -70,21 +70,22 @@ app.post('/webhook', function (req, res) {
       };
       rp(options)
         .then(function (object) {
-          console.log("call to neuhope server");dialogObject=dialogs;
-          
+          console.log("call to neuhope server");
+          dialogObject=object;
+          webhookReply = "Welcome from Heroku"
+          res.status(200).json({
+            source: 'webhook',
+            speech: webhookReply,
+            displayText: webhookReply
+          })
           
           
         })
         .catch(function (err) {
           // API call failed...
         });
-        */
-        webhookReply = "Welcome from Heroku"
-        res.status(200).json({
-          source: 'webhook',
-          speech: webhookReply,
-          displayText: webhookReply
-        })
+        
+        
         
       
     },
@@ -127,8 +128,8 @@ app.post('/webhook', function (req, res) {
         });
     },
     'registerUser': () => {
-      dialogObject=dialogs;
-      webhookReply = dialogObject.dialogs.messages[0].message;//dialogs.messages[0].message;
+      //dialogObject=dialogs;
+      webhookReply = dialogObject.messages[2].message;//dialogs.messages[0].message;
       res.status(200).json({
         source: 'webhook',
         speech: webhookReply,
