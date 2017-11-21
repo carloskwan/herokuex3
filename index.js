4101b9ba587a3019cd1501fd664188959837a196
carloskwan <carlos.araujo@kwantec.com>
2017-11-21 12:21:52 -0600

    make it better

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.post('/webhook', function (req, res) {
         .then(function (object) {
           
           dialogObject=object;
-          console.log(dialogObject.messages);
+          console.log(dialogObject["messages"]);
           webhookReply = "Welcome from Heroku"
           res.status(200).json({
             source: 'webhook',
